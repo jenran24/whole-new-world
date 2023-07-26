24a5d62cd01edef1e3fc7cf45c701b598fd9debc
Jenna Rank <jenrankster@gmail.com>
2023-07-26 16:49:55 -0700

    I forgot the parenthesis

--- app.js
+++ app.js
@@ -3,5 +3,7 @@
 console.log("I am in the HTML");
 alert("Welcome! I hope you love dogs!");
 
-const usersName = prompt(What is your favorite dog or animal?);
+const usersName = prompt("What is your favorite dog or animal?");
 console.log(usersName); 
+
+document.write("Howdy! I am so happy you're here! I also love" + usersName);
