3981b406e1b8d36058a57bec7c62bee5fd3e9ea5
Jenna Rank <jenrankster@gmail.com>
2023-07-26 17:02:16 -0700

    Updated JavaScript coding to be kind of funny with dynamics

--- app.js
+++ app.js
@@ -3,7 +3,15 @@
 console.log("I am in the HTML");
 alert("Welcome! I hope you love dogs!");
 
-const usersName = prompt("What is your favorite dog or animal?");
+const usersName = prompt("Would you rather have a dog or a cat?");
 console.log(usersName); 
 
-document.write("Howdy! I am so happy you're here! I also love" + usersName);
+document.write("Howdy! I am so happy you're here! I also love " + usersName);
+
+if (usersName == "dog" or "dogs" or "Dog" or "Dogs") {
+    alert("Me too! I love everything about dogs!")
+} else if (usersName == "cat" or "cats" or "Cat" or "Cats"){
+    alert("Cats are pretty cool, too! Taylor Swift has three of them!")
+} else {
+    alert("Oh, ok. I hope you at least have an animal you love!")
+}
