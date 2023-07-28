e44fad98f07e3823b551e6e506b5ee5e5bc295d1
Jenna Rank <jenrankster@gmail.com>
2023-07-28 15:42:52 -0700

    Trying to get the ratings and prompts to work with document write

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 'use strict';
 
 console.log("I am in the HTML");
-alert("Welcome! I hope you love Scout!");
+document.write("Welcome! I hope you love Scout!");
 
 function getUsername(); {
     const usersName = prompt("Would you rather have a dog or a cat?");
-
-    console.log(usersName)
-
     return usersName;
 }
-
-getUsername();
 
 console.log(getUsername());
 
 function greetUser(){
+    while(usersName == ""){
+        usersName = prompt("Oh no, I need an answer!! I need to knwo if you would rather have a cat or a dog?");
+    }
+
     if (usersName == "dog") {
-    alert("Me too! I love everything about dogs!")
+    document.write("Me too! I love everything about dogs!")
 } else if (usersName == "cat"){
-    alert("Cats are pretty cool, too! Taylor Swift has three of them!")
+    document.write("Cats are pretty cool, too! Taylor Swift has three of them!")
 } else {
-    alert("Oh, ok. I hope you at least have an animal you love!")
-}
+    document.write("Oh, ok. I hope you at least have an animal you love!")
+    }
 
 }
 
