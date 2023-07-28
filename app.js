f7ee5915e169305699e6d209892bb62837c0de0b
Jenna Rank <jenrankster@gmail.com>
2023-07-28 16:37:57 -0700

    changing the line for app?

--- app.js
+++ app.js
@@ -3,26 +3,26 @@
 console.log("I am in the HTML");
 alert("Welcome! I hope you love Scout!");
 
-const usersName = prompt("Would you rather have a dog or a cat?")
+const usersName = prompt("Would you rather have a dog or a cat?");
 console.log(usersName);
 
-function getUsername(); {
+function getUsersname(){
     const usersName = prompt("Would you rather have a dog or a cat?");
     return usersName;
 }
 
 function greetUser(){
-    let usersName = prompt("Would you rather have a dog or a cat?")
+    let usersName = prompt("Would you rather have a dog or a cat?");
     while(usersName == ""){
         usersName = prompt("Oh no, I need an answer!! I need to know if you would rather have a cat or a dog?");
     }
 
     if (usersName == "dog") {
-    document.write("Me too! I love everything about dogs!")
+    document.write("Me too! I love everything about dogs!");
 } else if (usersName == "cat"){
-    document.write("Cats are pretty cool, too! Taylor Swift has three of them!")
+    document.write("Cats are pretty cool, too! Taylor Swift has three of them!");
 } else {
-    document.write("Oh, ok. I hope you at least have an animal you love!")
+    document.write("Oh, ok. I hope you at least have an animal you love!");
     }
 
 }
@@ -30,9 +30,9 @@ function greetUser(){
 function rateMyPage(){
     let rating = prompt("How many stars would you rate my page? 1-5");
 
-    console.log(rating)
+    console.log(rating);
 
     for (let i = 0; i < rating; i++){
-        document.write('<img class="loop-img" src="scoutandsnow.jpg" alt="german shepherd sitting in the snow regally" />')
+        document.write('<img class="loop-img" src="scoutandsnow.jpg" alt="german shepherd sitting in the snow regally" />');
     }
 }
